5187547fd02bfb1c76c8fca349348ee77d8bb793
Timur <55026550+SVizor42@users.noreply.github.com>
2019-11-12 23:11:35 +0300

    Update main.cpp

--- 04/main.cpp
+++ 04/main.cpp
@@ -40,5 +40,17 @@ int main(int argc, const char* argv[])
 		std::cout << "m1 is not equal to m !" << std::endl;
 	}
 
+	const Matrix m2(rows, cols);
+	std::cout << "m2[2][2] = " << m2[2][2] << std::endl;
+	int y = m2[2][2];
+	m[1][0] = m2[2][2];
+	
+//	m2[2][2] = 1; // compilation error
+//	m2 *= 2; // compilation error
+
+	if (m2 == m1) {
+		std::cout << "Const matrix m2 is equal to m1 !" << std::endl;
+	}
+
 	return 0;
 }
