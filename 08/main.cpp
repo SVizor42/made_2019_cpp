d29dada728c931decf6b163d529d0a712d81e943
Timur <55026550+SVizor42@users.noreply.github.com>
2019-12-11 15:28:15 +0300

    Initial commit for task-08

---
+++ 08/main.cpp
@@ -0,0 +1,79 @@
+#include <cassert>
+#include <iostream>
+#include "vector.h"
+
+template<class T>
+void print_vector(Vector<T>& v) {
+	std::cout << "current_size = " << v.size() << std::endl;
+	std::cout << "items:" << std::endl;
+	for (auto it = v.begin(); it != v.end(); ++it) {
+		std::cout << *it << " ";
+	}
+    std::cout << std::endl;
+}
+
+int main() {
+	
+	std::cout << "Vector v:" << std::endl;
+	Vector<size_t> v(5, 42);
+    print_vector(v);	
+	
+	std::cout << "Vector v2 (copy of v):" << std::endl;
+	Vector<size_t> v2 = v;
+    print_vector(v2);
+	
+	std::cout << "Vector v3 from init list:" << std::endl;
+	Vector<int> v3 = {-3, 6, 0, 5, 5};
+    print_vector(v3);
+
+    
+	std::cout << "Clear v and push items again:" << std::endl;
+    v.clear();
+	for (size_t i = 1; i < 10; i++) {
+		v.push_back(i);
+	}
+    print_vector(v);
+
+    std::cout << "=====Resize test=====" << std::endl;	
+    std::cout << "Making vector v resize:" << std::endl;
+    v.resize(5);
+	std::cout << "current_size = " << v.size() << std::endl;
+    
+    v.resize(8, 100);
+	std::cout << "current_size = " << v.size() << std::endl;
+    
+    v.resize(12);
+	std::cout << "current_size = " << v.size() << std::endl;
+
+	std::cout << "Extract 2 items from v:" << std::endl;    
+    v.pop_back();
+    v.pop_back();
+    print_vector(v);
+
+    std::cout << "=====Reserve test=====" << std::endl;
+    Vector<int>::size_type sz;
+    Vector<int> foo;
+    sz = foo.capacity();
+    std::cout << "Making foo grow:" << std::endl;
+    for (size_t i = 0; i < 100; i++) {
+        foo.push_back(i);
+        if (sz != foo.capacity()) {
+        sz = foo.capacity();
+        std::cout << "capacity changed: " << sz << std::endl;;
+        }
+    }
+    
+    Vector<int> bar;
+    sz = bar.capacity();
+    bar.reserve(100);
+    std::cout << "Making bar grow:" << std::endl;
+    for (size_t i = 0; i < 100; i++) {
+        bar.push_back(i);
+        if (sz != bar.capacity()) {
+            sz = bar.capacity();
+            std::cout << "capacity changed: " << sz << std::endl;;
+        }
+    }
+
+	return 0;
+}
---
+++ 08/vector.h
@@ -0,0 +1,337 @@
+#ifndef vector_h
+#define vector_h
+
+#include <stdexcept>
+#include <limits>
+#include <iterator>
+#include <algorithm>
+
+const size_t DEFAULT_CAPACITY = 8;
+
+template <class T>
+class Iterator : public std::iterator<std::random_access_iterator_tag, T>
+{
+public:
+	using size_type = size_t;
+	using value_type = T;
+	using pointer = T*;
+	using reference = T&;
+	using const_reference = const T&;
+
+	explicit Iterator(pointer ptr) : ptr_(ptr) {};
+	Iterator(const Iterator& copied) = default;
+	Iterator& operator=(const Iterator& copied) = default;
+	Iterator(Iterator&& moved) = default;
+	Iterator& operator=(Iterator&& moved) = default;
+	~Iterator() = default;
+
+	bool operator==(const Iterator<value_type>& other) const {
+		return ptr_ == other.ptr_;
+	}
+
+	bool operator!=(const Iterator<value_type>& other) const {
+		return !(*this == other);
+	}
+
+	bool operator<(const Iterator<value_type>& other) const {
+		return ptr_ < other.ptr_;
+	}
+
+	bool operator>(const Iterator<value_type>& other) const {
+		return (other < *this);
+	}
+
+	bool operator<=(const Iterator<value_type>& other) const {
+		return (!(*this > other));
+	}
+
+	bool operator>=(const Iterator<value_type>& other) const {
+		return (!(*this < other));		
+	}
+
+	reference operator*() const {
+		return *ptr_;
+	}
+
+	pointer operator->() const {
+		return ptr_;
+	}
+
+	Iterator& operator++() {
+		++ptr_;
+		return *this;
+	}
+
+	Iterator& operator--() {
+		--ptr_;
+		return *this;
+	}
+	
+	Iterator operator++(int) {
+		Iterator temp(*this);
+		++ptr_;
+		return temp;
+	}
+
+	Iterator operator--(int) {
+		Iterator temp(*this);
+		--ptr_;
+		return temp;
+	}
+
+	Iterator& operator+=(size_type value) {
+		ptr_ += value;
+		return *this;
+	}
+
+	Iterator& operator-=(size_type value) {
+		ptr_ -= value;
+		return *this;
+	}
+
+	Iterator operator+(size_type value) const {
+		return Iterator(ptr_ + value);
+	}
+
+	Iterator operator-(size_type value) const {
+		return Iterator(ptr_ - value);
+	}
+
+private:
+	pointer ptr_;
+};
+
+template <class T, class Alloc = std::allocator<T> >
+class Vector {
+public:
+	using size_type = size_t;
+	using value_type = T;
+	using pointer = T*;
+	using reference = T&;
+	using const_reference = const T&;
+	using iterator = Iterator<T>;
+	using reverse_iterator = std::reverse_iterator<Iterator<T>>;
+	using allocator_type = Alloc;
+
+	Vector()
+		: alloc_(Alloc())
+		, size_(0)
+		, capacity_(DEFAULT_CAPACITY)
+		, data_(alloc_.allocate(capacity_))
+	{}
+
+	explicit Vector(size_type count) 
+		: alloc_(Alloc())
+		, size_(count)
+		, capacity_(std::max(count, DEFAULT_CAPACITY))
+		, data_(alloc_.allocate(capacity_))
+	{}
+
+	Vector(size_type count, const value_type& default_value)
+		: alloc_(Alloc())
+		, size_(count)
+		, capacity_(std::max(count, DEFAULT_CAPACITY))
+		, data_(alloc_.allocate(capacity_)) 
+	{
+		for (size_type i = 0; i < size_; i++) {
+			alloc_.construct(data_ + i, default_value);
+		}
+	}
+
+	Vector(std::initializer_list<value_type> init)
+		: alloc_(Alloc())
+		, size_(init.size())
+		, capacity_(std::max(init.size(), DEFAULT_CAPACITY))
+		, data_(alloc_.allocate(capacity_)) 
+	{
+		for (size_type i = 0; i < size_; i++) {
+			alloc_.construct(data_ + i, *(init.begin() + i));
+		}
+	}
+
+	Vector(const Vector& copied)
+		: alloc_(Alloc())
+		, size_(copied.size())
+		, capacity_(copied.capacity_)
+		, data_(alloc_.allocate(capacity_)) 	
+	{
+		for (size_type i = 0; i < size_; i++) {
+			alloc_.construct(data_ + i, copied.data_[i]);
+		}
+	}
+
+	Vector& operator=(const Vector& copied)	{
+		if (this == &copied) {
+			return *this;
+		}
+
+		if (data_) {
+			clear();
+			alloc_.deallocate(data_, capacity_);
+			capacity_ = 0;
+		}
+
+		size_ = copied.size();
+		capacity_ = copied.capacity_;
+		data_ = alloc_.allocate(capacity_);
+		for (size_type i = 0; i < size_; i++) {
+			alloc_.construct(data_ + i, copied.data_[i]);
+		}
+
+		return *this;
+	}
+
+	Vector(Vector&& moved) 
+		: alloc_(Alloc())
+		, size_(moved.size())
+		, capacity_(moved.capacity_)
+		, data_(moved.data_) 	
+	{
+		moved.data_ = nullptr;
+		moved.size_ = 0;
+		moved.capacity_ = 0;
+	}
+
+	Vector& operator=(Vector&& moved) {
+		if (this == &moved) {
+			return *this;
+		}
+
+		if (data_) {
+			clear();
+			alloc_.deallocate(data_, capacity_);
+			capacity_ = 0;
+		}
+
+		size_ = moved.size();
+		capacity_ = moved.capacity_;
+		data_ = moved.data_;
+		moved.data_ = nullptr;
+		moved.size_ = 0;
+		moved.capacity_ = 0;
+
+		return *this;				
+	}
+
+	~Vector() {
+		clear();
+		alloc_.deallocate(data_, capacity_);
+		data_ = nullptr;
+		capacity_ = 0;
+	}
+
+	reference operator[](size_type index) {
+		if (index >= size_) {
+			throw std::out_of_range("Vector index is out of range!");
+		}
+
+		return data_[index];
+	}
+
+	const_reference operator[](size_type index) const {
+		if (index >= size_) {
+			throw std::out_of_range("Vector index is out of range!");
+		}
+
+		return data_[index];
+	}
+
+	void push_back(value_type&& value) {
+		if (size_ == capacity_) {
+			reserve(2 * capacity_);
+		}
+		
+		alloc_.construct(data_ + size_, std::forward<value_type>(value));
+		size_++;
+	}
+
+	void push_back(const_reference value) {
+		if (size_ == capacity_) {
+			reserve(2 * capacity_);
+		}
+
+		alloc_.construct(data_ + size_, value);
+		size_++;
+	}
+
+	void pop_back() {
+		if (!empty()) {
+			alloc_.destroy(data_ + size_);
+			size_--;
+		}
+	}
+
+	bool empty() const noexcept {
+		return (size_ == 0);
+	}
+
+	size_type size() const noexcept {
+		return size_;
+	}
+	
+	size_type capacity() const noexcept {
+		return capacity_;
+	}
+
+	void clear() noexcept {
+		for (size_type i = 0; i < size_; i++) {
+			alloc_.destroy(data_ + i);
+		}
+		size_ = 0;		
+	}
+
+	iterator begin() noexcept {
+		return iterator(data_);
+	};
+
+	iterator end() noexcept {
+		return iterator(data_ + size_);		
+	}
+
+	reverse_iterator rbegin() noexcept {
+		return reverse_iterator(end());
+	}
+
+	reverse_iterator rend() noexcept {
+		return reverse_iterator(begin());
+	}
+
+    template<class... Args>
+	void resize(size_type new_size, Args... args) {
+		if (new_size != capacity_) {
+			reserve(new_size);
+
+			for (size_type i = size_; i < new_size; i++) {
+				alloc_.construct(data_ + i, args...);
+			}
+			for (size_type i = new_size; i < size_; i++) {
+				alloc_.destroy(data_ + i);
+			}
+
+			size_ = new_size;			
+		}
+	}
+
+	void reserve(size_type new_capacity) {
+		if (new_capacity > capacity_) {
+			pointer new_data = alloc_.allocate(new_capacity);
+			
+			for (size_type i = 0; i < size_; i++) {
+				alloc_.construct(new_data + i, *(data_ + i));
+				alloc_.destroy(data_ + i);
+			}
+			alloc_.deallocate(data_, capacity_);
+
+			capacity_ = new_capacity;
+			data_ = new_data;
+		}
+	}
+
+private:
+	Alloc alloc_;
+	size_type size_;
+	size_type capacity_;	
+	pointer data_;
+};
+
+#endif
